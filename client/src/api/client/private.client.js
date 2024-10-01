fbae14355fecebd7acf89227116fdc2963e11522
Harsh Kumar <harshkumarsingh54321@gmail.com>
2024-10-01 10:07:14 +0530

    frontend url changed

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://netflix-clone-api-psi.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
